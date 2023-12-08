86ab55d881c0105ec2ae5eb2073029df5c8a954a
Rifanuraa <rifanura12@gmail.com>
2023-12-09 00:06:26 +0700

    update route get data kamar by id_kos

--- routes/kamar.routes.js
+++ routes/kamar.routes.js
@@ -1,37 +1,49 @@
-const express = require('express');
-const kamarRoutes = express.Router();
-const {prisma} = require("../config/prisma");
+const express = require("express")
+const kamarRoutes = express.Router()
+const { prisma } = require("../config/prisma")
 
 //Menginput data kamar
 kamarRoutes.post("/", async (req, res) => {
-    const newKamar = await prisma.kamar.create({
-        data: {
-            imgUrl: req.body.imgUrl,
-            nama_kamar: req.body.nama_kamar,
-            harga_kamar: req.body.harga_kamar,
-        },
-    });
-    res.status(200).json({
-        message: "Data telah ditambahkan",
-        data: newKamar,
-    });
-});
+  const newKamar = await prisma.kamar.create({
+    data: {
+      imgUrl: req.body.imgUrl,
+      nama_kamar: req.body.nama_kamar,
+      harga_kamar: req.body.harga_kamar,
+    },
+  })
+  res.status(200).json({
+    message: "Data telah ditambahkan",
+    data: newKamar,
+  })
+})
 
-//get data kamar
-kamarRoutes.get("/", async (req, res) =>{
-    const kamar = await prisma.kamar.findMany();
-    res.status(200).send(kamar);
- });
+// get data kamar
+kamarRoutes.get("/", async (req, res) => {
+  const kamar = await prisma.kamar.findMany()
+  res.status(200).send(kamar)
+})
+
+//get data kamar by id_kos
+// http://localhost:3000/kamar/1
+kamarRoutes.get("/kost/:id_kos", async (req, res) => {
+  const { id_kos } = req.params
+  const kamar = await prisma.kamar.findMany({
+    where: {
+      id_kos: parseInt(id_kos),
+    },
+  })
+  res.status(200).send(kamar)
+})
 
 //mengambil kamar dengan id
-kamarRoutes.get("/:id", async (req, res) =>{
-    const kamar = await prisma.kamar.findUnique({
-        where:{
-            id: parseInt(req.params.id),
-        },
-    });
-    if (!kamar) res.status(404).send("Kamar tidak ditemukan");
-    else res.status(200).send(kamar);
- });
+kamarRoutes.get("/:id", async (req, res) => {
+  const kamar = await prisma.kamar.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  })
+  if (!kamar) res.status(404).send("Kamar tidak ditemukan")
+  else res.status(200).send(kamar)
+})
 
-module.exports = {kamarRoutes};
+module.exports = { kamarRoutes }
